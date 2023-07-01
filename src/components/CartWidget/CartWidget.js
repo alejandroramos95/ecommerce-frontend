36b4c7cd33844ce421418fd65259ee84c5cd8b16
Alejandro <alejandrorh295@hotmail.com>
2023-07-01 14:07:05 -0300

    PreEntrega1Ramos

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CartWidget() {
+  return (
+    <div>
+      <i class="bi bi-cart"></i>
+    </div>
+  );
+}
+
+export default CartWidget;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,24 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import CartWidget from "../CartWidget/CartWidget.js";
+
+function NavBar() {
+  return (
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">ARH</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#cart">
+              <CartWidget />
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default NavBar;
