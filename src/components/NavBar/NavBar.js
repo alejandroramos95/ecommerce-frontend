7b728952456befa57bd0a96376045ff91daed256
Alejandro <alejandrorh295@hotmail.com>
2023-07-01 14:13:07 -0300

    PreEntrega1Ramos/fixNavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -11,6 +11,8 @@ function NavBar() {
           <Navbar.Brand href="#home">ARH</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#register">Register</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
             <Nav.Link href="#cart">
               <CartWidget />
             </Nav.Link>
